4aef0b0a8e9d6175390e74a19c37370ffb7e0da6
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-02-25 17:58:48 +0100

    ai chat

---
+++ src/app copy/about/components/AIchat.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+const AIchat = () => {
+  return (
+    <div className='my-13'>
+      <div className='border border-gray-200 px-7 py-5 h-[60svh] overflow-auto flex flex-col'>
+        <div className='grow flex flex-col gap-4'>
+          <div>Not available in prod</div>
+        </div>
+        <form>
+          <div className='flex w-full gap-4'>
+            <input
+              className='bg-primary-light border border-gray-900 grow p-4 text-small-thin focus:outline-none focus:ring-2 focus:ring-gray-200 transition-all duration-300 ease-in-out'
+              placeholder='What is Aksels favourite food?'
+            />
+            <input
+              type='submit'
+              value='Send'
+              className='bg-primary-light border border-gray-900 p-4 text-small-thin hover:bg-gray-800 hover:text-gray-50 transition-all duration-300 ease-in-out'
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+export default AIchat
