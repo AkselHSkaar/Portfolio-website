6b2562011b55c7e96ac58c5702e5b215759163eb
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-02-29 18:38:20 +0100

    switched mailservice from nodemailer to resend

--- src/app/utils/sendemail.ts
+++ src/app/utils/sendemail.ts
@@ -1,19 +1,29 @@
-import { FormData } from '../contact/_components/ContactForm'
+'use server'
 
-export function sendEmail(data: FormData) {
-  const apiEndpoint = '/api/email'
+import { Resend } from 'resend'
 
-  fetch(apiEndpoint, {
-    method: 'POST',
-    body: JSON.stringify(data),
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export const sendEmail = async (formData: FormData) => {
+  const name = formData.get('name')
+  const senderEmail = formData.get('senderEmail')
+  const message = formData.get('message')
+
+  // if (!message || typeof message !== 'string') {
+  //   return {
+  //     error: 'Invalid message',
+  //   }
+  // }
+
+  //TOODO: Style email med react.email
+
+  resend.emails.send({
+    from: 'Kontakt skjema - Akselskaar.no <onboarding@resend.dev>',
+    to: 'akselhskaar@hotmail.no',
+    subject: `Kontakt skjema fra ${name}`,
+    reply_to: senderEmail as string,
+    html: `<h1>Ny melding fra kontakt skjema på akselskaar.no</h1>
+    <p>Medingen er fra: ${name}, ${senderEmail} </p>
+    <p>Melding: ${message}</p>`,
   })
-    .then((res) => res.json())
-
-    // .then((response) => {
-    //   alert(response.message)
-    // })
-
-    .catch((err) => {
-      console.log(err)
-    })
 }
