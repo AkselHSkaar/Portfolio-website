33d9c3d41328fa800aa788ec5f066853112b0fd0
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-03-01 00:58:27 +0100

    test

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,7 @@ import ContactForm from './_components/ContactForm'
 const page = () => {
   return (
     <Container>
-      <PageHero title='CONTACT ME' text='Contact me blblbldjbfdbje' />
+      <PageHero title='CONTACT ME' text='Contact me kids drugs' />
       <main>
         <ContactForm />
       </main>
