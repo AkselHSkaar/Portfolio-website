e9ae8dfb597c719d0ca82c67ec86fd09f3d939f6
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-03-02 02:48:30 +0100

    ProjectItem styling

--- src/app/(homepage)/ProjectsItem.tsx
+++ src/app/(homepage)/ProjectsItem.tsx
@@ -18,15 +18,17 @@ type ProjectsData = {
 const ProjectsItem = ({ data }: ProjectsItemProps) => {
   return (
     <Link href={`/${data.slug}`}>
-      <article className='grid grid-cols-2 grid-flow-row gap-4'>
-        <div className='relative col-span-2 aspect-video'>
+      <article className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-8 grid-flow-row gap-4 md:gap-x-7 md:gap-y-7'>
+        <div className='relative col-span-2 md:col-span-3 lg:col-span-8 md:row-start-1 aspect-video'>
           <Image src={data.thumbnailImg} alt='image' fill={true} />
         </div>
-        <header className='col-span-2 flex justify-between row-start-1'>
-          <p className='text-gray-500'>{data.title}</p>
-          <p className='text-right'>({data.year})</p>
+        <header className='col-span-2 md:col-span-1 lg:col-span-2 flex md:flex-col justify-between md:justify-start row-start-1 md:row-start-2 text-xsmall-regular md:text-small-regular lg:text-regular-regular xl:text-medium-regular 2xl:text-large-regular'>
+          <p className=''>
+            0{data.id} - {data.title}
+          </p>
+          <p className='text-right md:text-left'>({data.year})</p>
         </header>
-        <p className='col-span-2 text-secondary-light dark:text-secondary-dark'>
+        <p className='col-span-2 lg:col-span-5 text-gray-500 dark:text-gray-200'>
           {data.description}
         </p>
       </article>
