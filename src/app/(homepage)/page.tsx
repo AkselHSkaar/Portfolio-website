edb2d1fa03403a5757974cd1bd8265782e7fda36
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-03-02 01:56:42 +0100

    Hero on homepage

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -6,20 +6,20 @@ import ProjectsItem from './ProjectsItem'
 
 export default function Home() {
   return (
-    <>
-      <header className='mt-navbar-sm md:mt-navbar-md lg:mt-navbar-lg py-section-xs md:py-section-md lg:py-section-lg xl:py-section-xl 2xl:py-section-2xl'>
+    <main className='mt-navbar-sm md:mt-navbar-md lg:mt-navbar-lg'>
+      <header className='py-section-xs md:py-section-md lg:py-section-lg xl:py-section-xl 2xl:py-section-2xl'>
         <Container>
-          <div className='flex flex-col gap-7 lg:gap-9 xl:gap-13 2xl:gap-15'>
-            <div className='flex flex-col gap-4 md:gap-7 lg:gap-9 2xl:gap-11'>
+          <div className='flex flex-col gap-9 md:gap-11 xl:gap-14 2xl:gap-16'>
+            <div className='flex flex-col gap-5 md:gap-7 lg:gap-9 xl:gap-11 2xl:gap-13'>
               <h1 className='text-h6 sm:text-h5 md:text-h4 lg:text-h3 xl:text-h2 2xl:text-h1'>
                 HELLO, I&apos;M <br /> AKSEL SKAAR
               </h1>
-              <p className='text-secondary-light max-w-[21.375rem] md:max-w-full md:w-2/3 lg:w-1/2 text-small-thin md:text-regular-thin lg:text-large-thin xl:text-huge-thin 2xl:text-xhuge-thin'>
+              <p className='text-gray-500 dark:text-gray-200 max-w-[21.375rem] md:max-w-full md:w-2/3 lg:w-1/2 text-small-thin md:text-regular-thin lg:text-large-thin xl:text-huge-thin 2xl:text-xhuge-thin'>
                 I&apos;m a Oslo based UX/UI designer and web developer,
                 currently working at Aboveit.
               </p>
             </div>
-            <ArrowLink href='/'>CONTACT ME</ArrowLink>
+            <ArrowLink href='/contact'>CONTACT ME</ArrowLink>
           </div>
         </Container>
       </header>
@@ -31,10 +31,10 @@ export default function Home() {
       <section>
         <Container>
           <div className='py-section-xs md:py-section-md lg:py-section-lg xl:py-section-xl 2xl:py-section-2xl flex flex-col gap-7'>
-            <h2 className='text-xsmall-regular  md:text-small-regular lg:text-regular-regular xl:text-medium-regular 2xl:text-xlarge-regular'>
+            <h2 className='text-small-regular  md:text-regular-regular lg:text-medium-regular xl:text-medium-regular 2xl:text-xlarge-regular'>
               (ABOUT ME)
             </h2>
-            <div className='flex flex-col gap-5 lg:gap-11 text-secondary-light dark:text-secondary-dark text-xsmall-regular md:text-regular-regular lg:text-medium-regular xl:text-xlarge-regular 2xl:text-huge-regular'>
+            <div className='flex flex-col gap-5 lg:gap-11 text-gray-500 dark:text-gray-200'>
               <p>
                 I have a bacehlors degree in technology & interaction design for
                 Kristiania University College (2022). While studying i also
@@ -60,6 +60,6 @@ export default function Home() {
           </div>
         </Container>
       </section>
-    </>
+    </main>
   )
 }
