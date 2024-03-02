219a417a7e4eb0e6786608b7fc1ccf47413a6973
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-03-02 01:44:39 +0100

    ArrowLink style

--- src/components/ArrowLink.tsx
+++ src/components/ArrowLink.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import ArrowRight from './icons/ArrowRight'
 import ArrowUp from './icons/ArrowUp'
+import { link } from 'fs'
 
 type ArrowLinkProps = {
   href: string
@@ -10,19 +11,19 @@ type ArrowLinkProps = {
 
 const ArrowLink = ({ href, children, direction = 'right' }: ArrowLinkProps) => {
   return (
-    <div className='flex grow-0 text-xsmall-regular md:text-small-regular lg:text-regular-regular xl:text-medium-regular 2xl:text-xlarge-regular hover:text-gray-400 transition-colors duration-200 ease-in-out'>
-      <div className='flex flex-col gap-2'>
-        <Link href={href} className='flex gap-3 items-center'>
-          {direction === 'right' ? (
-            <ArrowRight className='fill-gray-800 size-5 lg:size-6 xl:size-7 2xl:size-8' />
-          ) : (
-            <ArrowUp className='fill-gray-800 size-5 lg:size-6 xl:size-7 2xl:size-8' />
-          )}
+    <div className='flex'>
+      <Link
+        href={href}
+        className='flex gap-3 pb-2 grow-0 text-xsmall-regular md:text-small-regular lg:text-regular-regular xl:text-medium-regular 2xl:text-xlarge-regular border-b border-gray-800 hover:text-gray-500 hover:border-gray-500 dark:border-gray-50 dark:hover:border-gray-200 dark:hover:text-gray-200'
+      >
+        {direction === 'right' ? (
+          <ArrowRight className='size-5 lg:size-6 xl:size-7 2xl:size-8' />
+        ) : (
+          <ArrowUp className='size-5 lg:size-6 2xl:size-7' />
+        )}
 
-          {children}
-        </Link>
-        <div className='h-[1px] lg:h-1 bg-gray-800'></div>
-      </div>
+        {children}
+      </Link>
     </div>
   )
 }
