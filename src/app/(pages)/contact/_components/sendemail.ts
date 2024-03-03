c02b3d0420825ef010cc62958ac17202990f8ea6
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-03-03 16:35:28 +0100

    add: ContactForm email styled with react-email

--- src/app/(pages)/contact/_components/sendemail.ts
+++ src/app/(pages)/contact/_components/sendemail.ts
@@ -1,8 +1,8 @@
 'use server'
 
-import { unstable_noStore as noStore } from 'next/cache'
 import { Resend } from 'resend'
-noStore()
+import ContactEmail from '@/email/ContactEmail'
+import React from 'react'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -11,21 +11,19 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get('senderEmail')
   const message = formData.get('message')
 
-  // if (!message || typeof message !== 'string') {
-  //   return {
-  //     error: 'Invalid message',
-  //   }
-  // }
-
-  //TOODO: Style email med react.email
-
-  resend.emails.send({
-    from: 'Kontakt skjema - Akselskaar.no <onboarding@resend.dev>',
-    to: 'akselhskaar@hotmail.no',
-    subject: `Kontakt skjema fra ${name}`,
-    reply_to: senderEmail as string,
-    html: `<h1>Ny melding fra kontakt skjema på akselskaar.no</h1>
-    <p>Medingen er fra: ${name}, ${senderEmail} </p>
-    <p>Melding: ${message}</p>`,
-  })
+  try {
+    await resend.emails.send({
+      from: `Ny melding fra ${name} <onboarding@resend.dev>`,
+      to: 'akselhskaar@hotmail.no',
+      subject: `Ny melding fra kontakt skjema på akselskaar.no`,
+      reply_to: senderEmail as string,
+      react: React.createElement(ContactEmail, {
+        name: name as string,
+        senderEmail: senderEmail as string,
+        message: message as string,
+      }),
+    })
+  } catch (error: unknown) {
+    console.log(error)
+  }
 }
