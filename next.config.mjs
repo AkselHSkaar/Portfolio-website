c82efdeeace18e17311fcca90302ff8de5b35bf6
Aksel Skaar <56083576+AkselHSkaar@users.noreply.github.com>
2024-03-09 15:58:22 +0100

    Redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.akselskaar.no/:path*',
+        permanent: true,
+        has: [
+          {
+            type: 'host',
+            value: 'akselskaar.no',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig
