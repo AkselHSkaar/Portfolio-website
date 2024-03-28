acec05cfd44855486ff8f3626a7c5c4a301b4f30
akselhskaar <akselhskaar@hotmail.no>
2024-03-28 20:56:30 +0100

    add: About me page

---
+++ src/app/(pages)/about/page.tsx
@@ -0,0 +1,13 @@
+import Container from '@components/shared/Container'
+import Chat from './_components/Chat'
+
+const page = () => {
+  return (
+    <>
+      <Container>
+        <Chat />
+      </Container>
+    </>
+  )
+}
+export default page
