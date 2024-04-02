294d51bc542c7db737ed36bb79f5a90793c1d4d3
akselhskaar <akselhskaar@hotmail.no>
2024-04-02 20:30:41 +0200

    fix: contact form with chacdn/ui components

--- src/app/contact/_components/ContactForm.tsx
+++ src/app/contact/_components/ContactForm.tsx
@@ -3,112 +3,134 @@
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { sendEmailAction } from '@actions/actions'
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  Form,
+  FormMessage,
+  FormDescription,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import { useState } from 'react'
-import Button from '@components/Button'
+import { Button } from '@/components/ui/button'
+// import Button from '@components/Button'
 import {
   contactFormSchema,
   TContactFormSchema,
 } from '@schemas/contactFormSchema'
+// import { Checkbox } from '@radix-ui/react-checkbox'
 
 const NewContactForm = () => {
-  const {
-    register,
-    formState: { errors },
-    trigger,
-    getValues,
-    reset,
-  } = useForm<TContactFormSchema>({
+  const form = useForm<TContactFormSchema>({
     resolver: zodResolver(contactFormSchema),
+    mode: 'onBlur',
+    defaultValues: {
+      name: '',
+      senderEmail: '',
+      message: '',
+      // accepted: false,
+    },
   })
 
   const [feedbackMessage, setFeedbackMessage] = useState<string>()
 
   return (
-    <form
-      action={async () => {
-        // Trigger to validate the form
-        const result = await trigger()
-        if (!result) return
+    <Form {...form}>
+      <form
+        action={async () => {
+          const result = await sendEmailAction(form.getValues())
+          setFeedbackMessage(result.message)
 
-        // Get form values and send to server action
-        const contactSubmission = getValues()
-        const feedbackMessage = await sendEmailAction(contactSubmission)
+          if (result.reset) {
+            form.reset()
+          }
+        }}
+        className='lg:col-span-5 xl:col-span-2'
+        autoComplete='on'
+      >
+        <div className='flex flex-col gap-7 text-small-thin'>
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full name</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Navn Etternavn'
+                    {...field}
+                    autoComplete='name'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        // Set the feedback message from the server
-        setFeedbackMessage(feedbackMessage.message)
+          <FormField
+            control={form.control}
+            name='senderEmail'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='example@email.com'
+                    {...field}
+                    autoComplete='email'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        // Reset the form if the server response is successful
-        if (feedbackMessage.reset) {
-          reset()
-        }
-      }}
-      className='lg:col-span-5 xl:col-span-2'
-      autoComplete='on'
-    >
-      <div className='flex flex-col gap-7'>
-        <div className='flex flex-col gap-3'>
-          <label className='flex flex-col gap-3'>
-            <span className='text-small-thin text-gray-700 dark:text-gray-100'>
-              Full name
-            </span>
-            <input
-              {...register('name')}
-              className='w-full outline-none rounded-none p-4 text-small-thin border border-gray-900 bg-gray-50 dark:bg-gray-800 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-400 placeholder:text-gray-500 transition-all duration-300 ease-in-out'
-              placeholder='John Doe'
-              autoComplete='name'
-            />
-          </label>
+          <FormField
+            control={form.control}
+            name='message'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder='Type your message here..'
+                    {...field}
+                    rows={4}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* <FormField
+            control={form.control}
+            name='accepted'
+            render={({ field }) => (
+              <FormItem className='flex flex-row gap-7'>
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel>I accept the privacy policy</FormLabel>
+              </FormItem>
+            )}
+          /> */}
 
-          {errors.name && (
-            <p className='text-small-thin'>{`${errors.name.message}`}</p>
+          {feedbackMessage && (
+            <p className='text-small-thin'>{feedbackMessage}</p>
           )}
+
+          <Button type='submit' className='self-end'>
+            Send
+          </Button>
         </div>
-
-        <div className='flex flex-col gap-7'>
-          <label className='flex flex-col gap-3'>
-            <span className='text-small-thin text-gray-700 dark:text-gray-100'>
-              E-mail
-            </span>
-            <input
-              {...register('senderEmail')}
-              className='w-full outline-none p-4 rounded-none text-small-thin border border-gray-900 bg-gray-50 dark:bg-gray-800 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-400 placeholder:text-gray-500 transition-all duration-300 ease-in-out'
-              placeholder='example@email.com'
-              autoComplete='email'
-            />
-          </label>
-
-          {errors.senderEmail && (
-            <p className='text-small-thin'>{`${errors.senderEmail.message}`}</p>
-          )}
-        </div>
-
-        <div className='flex flex-col gap-7'>
-          <label className='flex flex-col gap-3'>
-            <span className='text-small-thin text-gray-700 dark:text-gray-100'>
-              Message
-            </span>
-            <textarea
-              {...register('message')}
-              rows={4}
-              className='w-full outline-none rounded-none p-4 text-small-thin border border-gray-900 bg-gray-50 dark:bg-gray-800 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-400 placeholder:text-gray-500 transition-all duration-300 ease-in-out resize-none'
-              placeholder='Type your message here..'
-            />
-          </label>
-
-          {errors.message && (
-            <p className='text-small-thin'>{`${errors.message.message}`}</p>
-          )}
-        </div>
-
-        {feedbackMessage && (
-          <p className='text-small-thin'>{feedbackMessage}</p>
-        )}
-
-        <Button type='submit' pendingMessage='Sending..' className='self-end'>
-          Send
-        </Button>
-      </div>
-    </form>
+      </form>
+    </Form>
   )
 }
 export default NewContactForm
