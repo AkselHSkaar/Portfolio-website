b3217b2a6ab7f61ea98ec54e6ca9879fc5736f6b
akselhskaar <akselhskaar@hotmail.no>
2024-04-02 10:41:14 +0200

    fix: added www. to sitemap and robots.txt

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://akselskaar.no',
+      url: 'https://www.akselskaar.no',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://akselskaar.no/contact',
+      url: 'https://www.akselskaar.no/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
