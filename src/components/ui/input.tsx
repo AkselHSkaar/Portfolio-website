a9769c3887d36f2ade23e47adcdfe18e3aff326c
akselhskaar <akselhskaar@hotmail.no>
2024-04-03 23:29:08 +0200

    bugfix: removed scroll inside input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex w-full rounded-none border border-gray-300 bg-gray-0 p-4 text-small-thin ring-offset-gray-50 file:border-0 file:bg-transparent file:text-small-thin file:font-medium placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-800 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-400 dark:bg-gray-800 dark:ring-offset-gray-800 dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-50 transition-all duration-100 ease-in-out',
+          'flex w-full rounded-none border border-gray-300 bg-gray-0 py-4 px-4 h-12 text-small-thin ring-offset-gray-50 file:border-0 file:bg-transparent file:text-small-thin file:font-medium placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-800 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-400 dark:bg-gray-800 dark:ring-offset-gray-800 dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-50 transition-all duration-100 ease-in-out',
           className
         )}
         ref={ref}
