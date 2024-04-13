7195912e1c687a4caaa8f37be36683203dbccd7c
akselhskaar <akselhskaar@hotmail.no>
2024-04-13 17:17:35 +0200

    bugfix: payload config env name

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,55 +5,14 @@ import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload/config'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
-// import { Users, Pages, Media } from './collections'
+import { Users, Pages, Media } from './collections'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
   editor: lexicalEditor(),
-  // collections: [Users, Pages, Media],
-  collections: [
-    {
-      slug: 'users',
-      auth: true,
-      access: {
-        delete: () => false,
-        update: () => false,
-      },
-      fields: [],
-    },
-    {
-      slug: 'pages',
-      admin: {
-        useAsTitle: 'title',
-      },
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-        {
-          name: 'slug',
-          type: 'text',
-        },
-        {
-          name: 'content',
-          type: 'richText',
-        },
-      ],
-    },
-    {
-      slug: 'media',
-      upload: true,
-      fields: [
-        {
-          name: 'text',
-          type: 'text',
-        },
-      ],
-    },
-  ],
+  collections: [Users, Pages, Media],
   secret: process.env.PAYLOAD_SECRET || '',
 
   typescript: {
@@ -62,7 +21,7 @@ export default buildConfig({
 
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      connectionString: process.env.POSTGRES_URI || '',
     },
   }),
 
@@ -71,30 +30,30 @@ export default buildConfig({
   },
 
   admin: {
-    // user: Users.slug, (fra default)
-    autoLogin: {
-      email: 'dev@payloadcms.com',
-      password: 'test',
-      prefillOnly: true,
-    },
+    user: Users.slug,
+    // autoLogin: {
+    //   email: 'dev@payloadcms.com',
+    //   password: 'test',
+    //   prefillOnly: true,
+    // },
   },
 
-  async onInit(payload) {
-    const existingUsers = await payload.find({
-      collection: 'users',
-      limit: 1,
-    })
+  // async onInit(payload) {
+  //   const existingUsers = await payload.find({
+  //     collection: 'users',
+  //     limit: 1,
+  //   })
 
-    if (existingUsers.docs.length === 0) {
-      await payload.create({
-        collection: 'users',
-        data: {
-          email: 'dev@payloadcms.com',
-          password: 'test',
-        },
-      })
-    }
-  },
+  //   if (existingUsers.docs.length === 0) {
+  //     await payload.create({
+  //       collection: 'users',
+  //       data: {
+  //         email: 'dev@payloadcms.com',
+  //         password: 'test',
+  //       },
+  //     })
+  //   }
+  // },
   // Sharp is now an optional dependency -
   // if you want to resize images, crop, set focal point, etc.
   // make sure to install it and pass it to the config.
